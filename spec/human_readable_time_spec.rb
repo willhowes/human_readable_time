be803accc74424dc2c05f0912c3ed18c3f2003ff
Will Howes <williameldenhowes@gmail.com>
2019-09-03 07:57:22 +0100

    Passes test for single digit seconds given as input

--- spec/human_readable_time_spec.rb
+++ spec/human_readable_time_spec.rb
@@ -4,4 +4,8 @@ describe '#human_readable_time' do
   it 'will return "00:00:00" for an input of 0' do
     expect(human_readable_time(0)).to eq("00:00:00")
   end
+
+  it 'will return "00:00:05" for an input of 5' do
+    expect(human_readable_time(5)).to eq("00:00:05")
+  end
 end
